7a2849771163ecc908c1be3054e12e445d697cef
Padinsky <adrianpadillarojas@gmail.com>
2026-09-16 09:51:52 +0100

    Resolviendo problema 14

---
+++ problem-14/index.js
@@ -0,0 +1,40 @@
+function isEven(number) {
+    return number / 2;
+}
+
+function isOdd(number) {
+    return number * 3 + 1;
+}
+
+function selectSequence(number) {
+    if (number % 2 === 0) {
+        return isEven(number);
+    } else {
+        return isOdd(number);
+    }
+}
+
+function collatzSequence(number) {
+    let currentNum = number;
+    let counter = 1;
+    while (number !== 1) {
+        counter++;
+        number = selectSequence(number);
+    }
+    return counter;
+}
+
+function findLongestCollatzSequence(limit) {
+    let longestSequence = 0;
+    let longestSequenceNumber = 0;
+    for (let i = 1; i < limit; i++) {
+        const currentSequence = collatzSequence(i);
+        if (currentSequence > longestSequence) {
+            longestSequence = currentSequence;
+            longestSequenceNumber = i;
+        }
+    }
+    return { longestSequenceNumber, longestSequence};
+}
+
+console.log(findLongestCollatzSequence(1000000));
